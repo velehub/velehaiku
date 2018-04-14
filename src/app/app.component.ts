d648f17d68fa9ba797c06d52b356d891cef999ea
DESKTOP-016AAS8\velem <ach4bsns@gmail.com>
2018-04-14 15:29:03 +0300

    Added test echo button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClientModule }  from '@angular/common/http';
+import { HttpClient }  from '@angular/common/http';
+import { HttpHeaders }  from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'velehaiku';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  echoTest() {
+    console.log(1);
+    const url = 'https://velehaiku-server.herokuapp.com/echo?test=passed';
+    const options = {
+      withCredentials: true,
+    };
+    this.httpClient.get(url, options)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
